c9e1357f26ea74e53a32583791e1593f74efb329
Austin White <AustinWhite93@gmail.com>
2019-01-29 12:24:14 -0800

    switch input to use get_next_line

--- ms_loop.c
+++ ms_loop.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+/*
 static int		set_vals(char c, int *unmatched)
 {
 	if (IS_QUOTE(c) && *unmatched == 0)
@@ -51,7 +51,7 @@ static int		ms_read_line(char **line)
 	}
 	return (unmatched);
 }
-
+*/
 static char		**ms_split_line(char *line)
 {
 	char **split_line;
@@ -67,20 +67,13 @@ void			ms_loop(void)
 	char	*line;
 	char	**args;
 	int		status;
-	char	unmatched;
 
 	status = 1;
 	while (status)
 	{
 		args = NULL;
 		ft_putstr("$> ");
-		if ((unmatched = ms_read_line(&line)) > 0)
-		{
-			ft_putstr_fd("Unmatched ", STDERR_FILENO);
-			ft_putchar_fd(unmatched, STDERR_FILENO);
-			ft_putendl_fd("", STDERR_FILENO);
-			continue;
-		}
+		get_next_line(0, &line);
 		if (ft_strlen(line) != 0)
 			args = ms_split_line(line);
 		if (args != NULL)
