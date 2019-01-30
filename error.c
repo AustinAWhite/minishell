d75ef51d7f7b4563de4960c87122dc369dd78dc5
Austin White <AustinWhite93@gmail.com>
2019-01-29 16:03:49 -0800

    unmatched quote error

--- error.c
+++ error.c
@@ -31,3 +31,10 @@ void	ms_error_envv(char *name)
 	ft_putstr_fd(name, STDERR_FILENO);
 	ft_putendl_fd(": undefined variable", STDERR_FILENO);
 }
+
+void	ms_unmatched_quotes(int unmatched)
+{
+	ft_putstr_fd("Unmatched: ", STDERR_FILENO);
+	ft_putchar_fd(unmatched, STDERR_FILENO);
+	ft_putendl_fd("", STDERR_FILENO);
+}
