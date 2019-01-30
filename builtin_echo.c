f9ec1554c5f5fc42acf09747329b231b18f1b246
Austin White <AustinWhite93@gmail.com>
2019-01-29 16:03:19 -0800

    prints quoted text properly

--- builtin_echo.c
+++ builtin_echo.c
@@ -12,10 +12,76 @@
 
 #include "minishell.h"
 
+char		*join_args(char **args, int n_flag)
+{
+	int i;
+	char *joined;
+	char *tmp;
+
+	(n_flag) ? (i = 1) : (i = 0);
+	joined = ft_strnew(1);
+	while (args[++i])
+	{
+		tmp = ft_strjoin(joined, args[i]);
+		free(joined);
+		joined = tmp;
+		if (args[i + 1])
+		{
+			tmp = ft_strjoin(joined, " ");
+			free(joined);
+			joined = tmp;
+		}
+	}
+	return (joined);
+}
+
+static int		set_vals(char c, int *unmatched)
+{
+	if (IS_QUOTE(c) && *unmatched == 0)
+	{
+		*unmatched = c;
+		return (1);
+	}
+	if (IS_QUOTE(c) && c == *unmatched)
+	{
+		*unmatched = 0;
+		return (1);
+	}
+	return (0);
+}
+
+static int	print_args(char **args, int n_flag)
+{
+	int i;
+	int unmatched;
+	char *joined;
+	char *formatted;
+	char *tmp;
+
+	i = -1;
+	unmatched = 0;
+	joined = join_args(args, n_flag);
+	formatted = ft_strnew(1);
+	while (joined[++i])
+	{
+		if ((set_vals(joined[i], &unmatched)) > 0)
+			continue;
+		if (joined[i] != '\\')
+		{
+			tmp = ft_strchrjoin(formatted, joined[i]);
+			free(formatted);
+			formatted = tmp;
+		}
+	}
+	(unmatched) ? (ms_unmatched_quotes(unmatched)) : (ft_putstr(formatted));
+	return (unmatched);
+}
+
 int			ms_builtin_echo(char **args)
 {
 	int i;
 	int n_flag;
+	int unmatched;
 
 	i = 0;
 	n_flag = 0;
@@ -29,13 +95,8 @@ int			ms_builtin_echo(char **args)
 		n_flag = 1;
 		i++;
 	}
-	while (args[++i])
-	{
-		ft_putstr(args[i]);
-		if (args[i + 1])
-			ft_putchar(' ');
-	}
-	if (!n_flag)
+	unmatched = print_args(args, n_flag);
+	if (!n_flag && unmatched == 0)
 		ft_putchar('\n');
 	return (1);
 }
